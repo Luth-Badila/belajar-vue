7852617a0edac3a6766cfb32da45151a7285ba2e
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-11-03 22:36:00 +0700

    added mouseevent with vue.js

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const app = Vue.createApp({
       title: "The Final Episode",
       author: "Nikola Zigic",
       age: 39,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
