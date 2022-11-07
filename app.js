331180f08d5e8c59360d9f9edc718d3e73896fcf
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-11-07 22:18:56 +0700

    added attribute binding

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://www.youtube.com",
       showBooks: true,
       books: [
         {
           title: "name of the window",
           author: "nikola zigic",
+          img: "assets/bgIMG.png",
         },
         {
           title: "dragon of metal",
           author: "lee chung young",
+          img: "assets/bgIMGGreen.png",
         },
         {
           title: "metal slug",
           author: "marko",
+          img: "assets/bgIMGYellow.png",
         },
       ],
     };
